59ceeec30f228626c587d68b87e6afb8d08dd83e
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2024-10-08 11:29:36 +0000

    fix: Change implementation to see whether it passes or not

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,70 +1,17 @@
 #!/usr/bin/python3
 """ This module definies a canUnlockAll function """
 
-
-class Graph:
-    """ Defines a graph"""
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = [[] for _ in range(self.V)]
-
-    def add_edge(self, u, v):
-        """ Adds an edge or relation between two vertices """
-        self.graph[u].append(v)
-        self.graph[v].append(u)
-
-    def dfs(self, start):
-        """ Depth-first Search implementation """
-        visited = [False] * self.V
-        stack = []
-
-        stack.append(start)
-        visited[start] = True
-
-        while stack:
-            current = stack.pop()
-
-            for neighbor in self.graph[current]:
-                if not visited[neighbor]:
-                    stack.append(neighbor)
-                    visited[neighbor] = True
-
-        return visited
-
-
 def canUnlockAll(boxes):
-    """Verifies whether boxes can be unlocked
-
-    Args:
-        boxes (list): The list containing the boxes to be verified.
-    Returns:
-        True if all the boxes can be unlocked, False otherwise.
+    """ Checks whether all the boxes can be unlocked.
     """
     size = len(boxes)
-    graph = Graph(size)
-
-    for i in range(0, size):
-        for key in boxes[i]:
-            if 0 <= key < size:
-                graph.add_edge(i, key)
-
-    visited = graph.dfs(0)
-
-    return size == sum(visited)
-
-
-def toGraph(boxes):
-    """Converts a list of list into a graph.
-
-    Args:
-        boxes (list): The list to be converted.
-    Returns:
-        Agraph containing a all the vertices from the list.
-    """
-    graph = Graph(len(boxes))
-    for i in range(0, len(boxes)):
-        for key in boxes[i]:
-            if key < len(boxes):
-                graph.add_edge(i, key)
-
-    return graph
+    visited = set([0])
+    not_visited = set(boxes[0]).difference(set([0]))
+    while len(not_visited) > 0:
+        idx = not_visited.pop()
+        if not idx or idx >= size or idx < 0:
+            continue
+        if idx not in visited:
+            not_visited = not_visited.union(boxes[idx])
+            visited.add(idx)
+    return size == len(visited)
