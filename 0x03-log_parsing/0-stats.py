b3e379125e513a4d6e95cacc7babb7ad53a39d5b
Edmilson de Azevedo Cassecasse <55765693+eacassecasse@users.noreply.github.com>
2024-10-23 15:32:55 +0000

    fix:Removing Walrus operator and f-strings o make it compatible with python 3.4.3

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
+""" This module parses a log and prints metrics. """
 import re
 
 
-def parse_input(log_line: str) -> dict:
+def parse_input(log_line):
     """
     Extracts IP address, status code, and size from a log line in a specific
     format and returns them in a dictionary.
@@ -16,25 +16,27 @@ def parse_input(log_line: str) -> dict:
         r'(?P<status>\d{3}) '
         r'(?P<size>\d+)'
     )
-
-    if match := re.match(pattern, log_line):
-        result[match["ip"]] = {
-            "status": match["status"],
-            "size": match["size"]
+    
+    match = re.match(pattern, log_line)
+    
+    if match:
+        result[match.group("ip")] = {
+            "status": match.group("status"),
+            "size": match.group("size")
             }
 
     return result
 
 
-def print_metrics(file_size: int, occurrences: dict):
+def print_metrics(file_size, occurrences):
     """
     Takes a file size and a dictionary of occurrences, and prints out the
     file size and each key-value status occurrence if the value is not empty.
     """
-    print(f"File size: {file_size}")
+    print("File size".format(file_size))
     for k, v in occurrences.items():
         if v:
-            print(f"{k}: {v}")
+            print("{}: {}".format(k, v))
 
 
 if __name__ == '__main__':
