2b5adc0606c1fc8a58e60ec93f262cc41d259011
Edmilson Cassecasse <>
2024-11-15 03:32:25 +0200

    fix: Make simple improvements to the code implementation

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,29 +1,24 @@
 #!/usr/bin/node
-
 const request = require('request');
-
 const BASE_URL = 'https://swapi-api.alx-tools.com/api';
 
 if (process.argv.length > 2) {
-  request(`${BASE_URL}/films/${process.argv[2]}`, (error, _, body) => {
+  request(`${BASE_URL}/films/${process.argv[2]}/`, (error, _, body) => {
     if (error) {
       console.log(error);
     }
     const characters = JSON.parse(body).characters;
-    const promises = characters.map((characterUrl) => {
-      return new Promise((resolve, reject) => {
+    const promises = characters.map(characterUrl =>
+      new Promise((resolve, reject) => {
         request(characterUrl, (err, _, data) => {
           if (err) {
             reject(err);
           }
           resolve(JSON.parse(data).name);
         });
-      });
-    });
+      }));
     Promise.all(promises)
-      .then((characterNames) => {
-        console.log(characterNames.join('\n'));
-      })
-      .catch((err) => console.log(err));
+      .then(characterNames => console.log(characterNames.join('\n')))
+      .catch(err => console.log(err));
   });
 }
