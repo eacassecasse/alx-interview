54b1b0ebbb60fc6ba35a3eafd8e4b5f8f4681cdd
Edmilson Cassecasse <>
2024-12-05 09:51:29 +0200

    fix: Updating the implementation to consider other shapes and work even when there are lakes

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,20 +13,22 @@ def island_perimeter(grid):
         return
     if len(grid) > 100 or len(grid[0]) > 100:
         return
-    island = []
 
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == 1:
-                island.append(grid[i][j])
-
-    if len(island) == 0:
-        return 0
     perimeter = 0
-    for i in range(len(island)):
-        if i == 0 or i == len(island) - 1:
-            perimeter += 3 * island[i]
-        else:
-            perimeter += 2 * island[i]
+    rows = len(grid)
+
+    for i, row in enumerate(grid):
+        cols = len(row)
+        for j, cell in enumerate(row):
+            if cell == 1:
+                if i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0):
+                    perimeter += 1
+                if i == rows - 1 or (
+                        len(grid[i + 1]) > j and grid[i + 1][j] == 0):
+                    perimeter += 1
+                if j == 0 or row[j - 1] == 0:
+                    perimeter += 1
+                if j == cols - 1 or (cols > j + 1 and row[j + 1] == 0):
+                    perimeter += 1
 
     return perimeter
